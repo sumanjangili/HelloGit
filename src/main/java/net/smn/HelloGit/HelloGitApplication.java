fab6c725858a3d2356d41f0cc99d45c6cec9accf
Suman Jangili <sumanjangili@Sumans-MacBook-Air.local>
2022-11-17 16:22:58 +0530

    initial commit

---
+++ src/main/java/net/smn/HelloGit/HelloGitApplication.java
@@ -0,0 +1,15 @@
+package net.smn.HelloGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloGitApplication.class, args);
+		System.out.println("this is suman to test the git hub");
+	}
+
+}
